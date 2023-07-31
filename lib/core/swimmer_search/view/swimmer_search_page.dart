8cb817c1b0c6aa042284abfd2477c68c8e1fcbbe
Nick Matthews <nickd.mf7@gmail.com>
2023-07-31 16:06:28 -0500

    branding spiked out

--- lib/core/swimmer_search/view/swimmer_search_page.dart
+++ lib/core/swimmer_search/view/swimmer_search_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ccswim_viz/theme.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:ccswim_viz/core/swimmer_search/swimmer_search.dart';
-import '';
 import 'package:ccswims_repository/ccswims_repository.dart';
 
 import 'swimmer_search_view.dart';
@@ -14,8 +14,9 @@ class SwimmerSearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Swimmer Search"),
+        title: const _AppBarTitle(),
         centerTitle: false,
+        backgroundColor: Colors.grey[100],
       ),
       body: Padding(
         padding: const EdgeInsets.all(4.0),
@@ -31,6 +32,27 @@ class SwimmerSearchPage extends StatelessWidget {
             child: const SwimmerSearchView(),
         ),
       ),
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset('assets/logo/logo_blue.png', fit: BoxFit.contain, height: 40),
+        const SizedBox(width: 12.0),
+        Text(
+          "CCSwimViz",
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+            color: neutral[3],
+            fontWeight: FontWeight.normal,
+          )
+        ),
+      ],
     );
   }
 }
